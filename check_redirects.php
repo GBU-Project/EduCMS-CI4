b09640f12af741ef4fab856618c50949a1c8e507
GBU-Project <abushaffa03@gmail.com>
2026-08-11 21:23:21 +0700

    fix(cleanup): remove sensitive debug scripts and build artifact logs

--- check_redirects.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$m = new mysqli('127.0.0.1', 'root', '', 'educms');
-$res = $m->query("SELECT * FROM redirects");
-echo "REDIRECTS IN DB:\n";
-while ($row = $res->fetch_assoc()) {
-    echo "ID: {$row['id']}, Source: {$row['source_url']}, Target: {$row['target_url']}, Type: {$row['redirect_type']}\n";
-}
--- debug_307.php
+++
@@ -1,8 +0,0 @@
-<?php
-$ch = curl_init('http://localhost/educms/public/admin/login');
-curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
-$res = curl_exec($ch);
-file_put_contents('header_out.txt', $res);
-echo "DONE writing header_out.txt, length: " . strlen($res);
